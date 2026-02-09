274e248e6b018ae9fa62eeb7361b7bc6f1a25d93
Mello <johnmelloregencia9@gmail.com>
2026-02-09 17:37:32 +0800

    fix template path for vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Tell Flask to look for templates in the parent directory
+app = Flask(__name__, template_folder='../templates')
 
 @app.route('/')
 def home():
